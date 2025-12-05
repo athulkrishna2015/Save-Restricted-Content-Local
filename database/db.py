b8c3243ebec7872e10c22c4871d477d746acac2b
Athulkrishna <35728672+athulkrishna2015@users.noreply.github.com>
2025-12-05 15:45:33 +0530

    Switch user data storage to local JSON file
    Refactor database class to use local JSON file for user data storage instead of MongoDB.

--- database/db.py
+++ database/db.py
@@ -1,68 +1,98 @@
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+import json
+import os
+import asyncio
 
-import motor.motor_asyncio
-from config import DB_NAME, DB_URI
+# Local JSON file to store user data
+DB_FILE = "database/users.json"
 
 class Database:
-    
-    def __init__(self, uri, database_name):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users
+    def __init__(self, uri=None, database_name=None):
+        # uri and database_name are kept for compatibility with the existing init signature
+        self.db_file = DB_FILE
+        if not os.path.exists(self.db_file):
+            with open(self.db_file, 'w') as f:
+                json.dump({}, f)
+        self.lock = asyncio.Lock()
 
-    def new_user(self, id, name):
-        return dict(
-            id = id,
-            name = name,
-            session = None,
-            api_id = None,
-            api_hash = None,
-        )
-    
+    def _load(self):
+        try:
+            with open(self.db_file, 'r') as f:
+                return json.load(f)
+        except (json.JSONDecodeError, FileNotFoundError):
+            return {}
+
+    def _save(self, data):
+        with open(self.db_file, 'w') as f:
+            json.dump(data, f, indent=4)
+
     async def add_user(self, id, name):
-        user = self.new_user(id, name)
-        await self.col.insert_one(user)
-    
+        async with self.lock:
+            data = self._load()
+            if str(id) not in data:
+                data[str(id)] = {
+                    "id": id,
+                    "name": name,
+                    "session": None,
+                    "api_id": None,
+                    "api_hash": None
+                }
+                self._save(data)
+
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id':int(id)})
-        return bool(user)
-    
+        data = self._load()
+        return str(id) in data
+
     async def total_users_count(self):
-        count = await self.col.count_documents({})
-        return count
+        data = self._load()
+        return len(data)
 
     async def get_all_users(self):
-        return self.col.find({})
+        data = self._load()
+        # Return an async generator to satisfy 'async for' loops in the bot code
+        async def async_gen():
+            for user in data.values():
+                yield user
+        return async_gen()
 
     async def delete_user(self, user_id):
-        await self.col.delete_many({'id': int(user_id)})
+        async with self.lock:
+            data = self._load()
+            if str(user_id) in data:
+                del data[str(user_id)]
+                self._save(data)
 
     async def set_session(self, id, session):
-        await self.col.update_one({'id': int(id)}, {'$set': {'session': session}})
+        async with self.lock:
+            data = self._load()
+            if str(id) in data:
+                data[str(id)]['session'] = session
+                self._save(data)
 
     async def get_session(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('session')
+        data = self._load()
+        return data.get(str(id), {}).get('session')
 
     async def set_api_id(self, id, api_id):
-        await self.col.update_one({'id': int(id)}, {'$set': {'api_id': api_id}})
+        async with self.lock:
+            data = self._load()
+            if str(id) in data:
+                data[str(id)]['api_id'] = api_id
+                self._save(data)
 
     async def get_api_id(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('api_id')
+        data = self._load()
+        return data.get(str(id), {}).get('api_id')
 
     async def set_api_hash(self, id, api_hash):
-        await self.col.update_one({'id': int(id)}, {'$set': {'api_hash': api_hash}})
+        async with self.lock:
+            data = self._load()
+            if str(id) in data:
+                data[str(id)]['api_hash'] = api_hash
+                self._save(data)
 
     async def get_api_hash(self, id):
-        user = await self.col.find_one({'id': int(id)})
-        return user.get('api_hash')
+        data = self._load()
+        return data.get(str(id), {}).get('api_hash')
 
-db = Database(DB_URI, "TechVJDemoBot")
-
-# Don't Remove Credit @VJ_Bots
-# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-                                       
+# Initialize the database instance (arguments are ignored)
+db = Database("dummy_uri", "dummy_name")
